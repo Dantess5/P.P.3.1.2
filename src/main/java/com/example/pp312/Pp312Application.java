a2504dffe17a2ba34fb95789b1eb27b31f595dd0
Dantess5 <nesterenko449@gmail.com>
2023-01-24 00:52:40 +0300

    !

--- src/main/java/com/example/pp312/Pp312Application.java
+++ src/main/java/com/example/pp312/Pp312Application.java
@@ -2,17 +2,11 @@ package com.example.pp312;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.filter.HiddenHttpMethodFilter;
 
 @SpringBootApplication
 public class Pp312Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Pp312Application.class, args);
-    }
-    @Bean
-    HiddenHttpMethodFilter hiddenHttpMethodFilter() {
-        return new HiddenHttpMethodFilter();
     }
 }
